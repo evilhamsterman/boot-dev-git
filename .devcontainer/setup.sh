270f6548a7e2b89edcd23585c4419f01c4c96d6c
Dan Mills <evilhamsterman@gmail.com>
2024-05-17 00:23:24 +0000

    Add bootdev and tailscale

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -f /workspaces/bootdev.yaml ]; then
+    echo "Creating /workspaces/bootdev.yaml"
+    touch /workspaces/bootdev.yaml
+fi
+
+if [ ! -L $HOME/.bootdev.yaml ]; then
+    echo "Creating symbolic link from /workspaces/bootdev.yaml to $HOME/.bootdev.yaml"
+    ln -s /workspaces/bootdev.yaml $HOME/.bootdev.yaml
+fi
+
+if ! command -v bootdev &> /dev/null; then
+    echo "Installing bootdev"
+    go install github.com/bootdotdev/bootdev@latest
+fi
